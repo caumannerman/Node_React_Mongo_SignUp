6635767cfb84d0748b2a86f8b01cea3f3189a252
Caumannerman <uigohiiming2@gmail.com>
2020-12-13 11:54:32 +0900

    Add Sign-Up function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
@@ -11,6 +18,20 @@ mongoose.connect('mongodb+srv://yan:first1@cluster0.hqovm.mongodb.net/<dbname>?r
 
 
 app.get('/', (req, res) => res.send('Hello world!'))
+
+
+app.post('/register', (req, res) => {
+  //회원가입 시 필요한 정보들을 client에서 가져오면, 그것들을 데이터 베이스에 넣어준다.
+
+      const user = new User(req.body)
+
+      user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+          success: true
+        })
+      })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
